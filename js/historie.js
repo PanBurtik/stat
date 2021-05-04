b78eb0e1069228f764ea2d65a6da4db16917b0bf
PanBurtik <panburticek@seznam.cz>
2021-05-04 14:34:47 +0200

    historie2

--- js/historie.js
+++ js/historie.js
@@ -1,7 +1,7 @@
 const heroes = [
     {
        "name":"Texas State Capitol",
-       "built":"Otevřeno: 21. dubna 1888",
+       "built":"Postaveno : 21. dubna 1888",
        "biography":"Texas State Capitol je Capitol budova a sídlo vlády amerického státu Texas. Nachází se v centru Austinu v Texasu, struktura obsahuje kanceláře a komory texaské legislativy a guvernéra Texasu. Navržený v roce 1881 architektem Elijahem E. Myersem, byl postaven v letech 1882 až 1888 pod vedením stavebního inženýra Reubena Lindsay Walkera. V roce 1993 byla dokončena podzemní přístavba ve výši 75 milionů USD. Budova byla přidána do Národního registru historických míst v roce 1970 a v roce 1986 byla uznána jako národní kulturní památka.",
        "portraits":[
           "state-capitol.jpg",
@@ -14,10 +14,8 @@ const heroes = [
         "built":"7.3.1850",
         "biography":"Fort Worth Stockyards je historická čtvrť, která se nachází ve městě Fort Worth v Texasu, severně od centrální obchodní čtvrti. Část 98 ha (40 ha) zahrnující velkou část okresu byla v roce 1976 uvedena v národním registru historických míst jako Fort Worth Stockyards Historic District. Je držitelem bývalého trhu s dobytkem, který fungoval pod různými vlastníky od roku 1866.",
         "portraits":[
-           "tgm-01.png",
-           "tgm-02.png",
-           "tgm-03.jpg",
-           "masaryk-capek.gif"
+           "fort-worth-stockyards.jpg",
+           "fort-worth-stockyards-2.jpg"
         ],
         "online":"https://en.wikipedia.org/wiki/Fort_Worth_Stockyards"
      },
@@ -26,53 +24,50 @@ const heroes = [
         "built":"5.10.1936",
         "biography":"Bitva o Alamo (23. února - 06.3.1836) byl klíčovou událostí v texaské revoluci . Po 13-ti denní obležení , mexičtí vojáci pod prezident General Antonio López de Santa Anna kultivoval Alamo mise u San Antonio de Bexar (současný San Antonio , Texas , USA), zabíjet většinu Texians a Tejanos dovnitř. Krutost Santa Anny během bitvy inspirovala mnoho Texianů a Tejanos ke vstupu do Texianské armády. Povzbuzení touhou po pomstě Texejci porazili mexickou armádu v bitvě u San Jacinto, 21. dubna 1836, ukončující povstání.",
         "portraits":[
-           "havel-01.jpg",
-           "havel-02.jpg",
-           "havel-03.jpg"
+           "Alamo.jpg",
+           "Alamo-2.jpg"
         ],
-        "online":"https://cs.wikipedia.org/wiki/V%C3%A1clav_Havel"
+        "online":"https://en.wikipedia.org/wiki/Battle_of_the_Alamo"
      },
      {
         "name":"Quinta Mazatlan",
         "built":"28.3.1592",
         "biography":"World Birding centrum je oficiální titul daný kombinovaným devíti parků a chráněných krajinných oblastí Rio Grande Valley oblasti Texasu spravuje partnerství s Texas parků a divoké zvěře odboru, do Spojených států Fish and Wildlife Service, a místní komunity v v nichž sídlí parky. Uvedenou misí Světového centra pro ptáky je „chránit přirozené prostředí a současně zvýšit porozumění a uznání ptáků a volně žijících živočichů,“ s dalším důrazem na podporu místního hospodářského rozvoje prostřednictvím ekoturistiky.",
         "portraits":[
-           "komensky-01.jpg",
-           "komensky-02.jpg",
+           "quinta-mazatlan.jpg",
+           "quinta-mazatlan-2.jpg"
         ],
-        "online":"https://cs.wikipedia.org/wiki/Jan_Amos_Komensk%C3%BD"
+        "online":"https://en.wikipedia.org/wiki/World_Birding_Center"
      },
      {
          "name":"Mission San Jose",
-         "built":"přibližně 1370",
+         "built":"Postaveno : 1768",
          "biography":"Mission San José je španělská mise umístěná v dnešním městě Fremont v Kalifornii. Byla založena 11. června 1797 františkánským řádem a byla čtrnáctou španělskou misí založenou v Kalifornii. Mise je jmenovkou misionářské čtvrti San José ve Fremontu, která byla samostatným městem zahrnutým do města, když byla založena v roce 1957. Mise vstoupila do dlouhého období postupného úpadku po mexickém sekularizačním aktu z roku 1833. Poté, co utrpěl úpadek, zanedbávání a zemětřesení, byla většina mise v troskách. Obnova v mezidobí rekonstruovala mnoho původních staveb. Starý kostel mise zůstane v použití jako kaple svatého Josefa katolické církve, farnosti na diecézi Oakland. V muzeu je také návštěvnické centrum, muzeum a prezentace, která vypráví o historii mise.",
          "portraits":[
-            "hus-01.jpg",
-            "hus-02.jpg",
-            "hus-03.jpg"
+            "kostel.JPG",
+            "kostel-2.JPG"
          ],
-         "online":"https://cs.wikipedia.org/wiki/Jan_Hus"
+         "online":"https://en.wikipedia.org/wiki/Mission_San_Jos%C3%A9_(Texas)"
       },
       {
          "name":"Fort Worth Botanic Garden",
-         "built":"8.9.1841",
+         "built":"Postavena : 1934",
          "biography":"Fort Worth Botanic Garden (110 akrů) je botanická zahrada se nachází na 3220 Botanic Garden Boulevard, Fort Worth v Texasu. Zahrada byla založena v roce 1934 a je nejstarší významnou botanickou zahradou v Texasu. Nachází se v samém srdci kulturní čtvrti. Nejoblíbenější oblasti jsou The Fuller, Rose a Japanese Gardens.",
          "portraits":[
-            "dvorak-01.jpg",
-            "dvorak-02.jpg",
+            "garden.jpg",
+            "garden-2.jpg"
          ],
-         "online":"https://cs.wikipedia.org/wiki/Anton%C3%ADn_Dvo%C5%99%C3%A1k"
+         "online":"https://en.wikipedia.org/wiki/Fort_Worth_Botanic_Garden"
       },
       {
         "name":"National Ranching Heritage Center",
         "built":"9.1.1890",
         "biography":"National Ranchigem Heritage Center, je muzeum z farmaření historie, který se nachází na kampusu Texas Tech University v Lubbock, Texas. NRHC představuje téměř padesát autentických rančových budov z konce 18. až poloviny 20. století. Mezi tyto stavby patří železniční nádraží , usedlosti, stodola, kovárna, školní dům, větrné mlýny a další historické stavby. Jeden si prohlíží exponáty prostřednictvím vlastní prohlídky s průvodcem. Pro veřejnost je zdarma.",
         "portraits":[
-           "capek-01.jpg",
-           "masaryk-capek.gif",
-           "capkove.jpg"
+           "farm.jpg",
+           "farm-2.jpg"
         ],
-        "online":"https://cs.wikipedia.org/wiki/Karel_%C4%8Capek"
+        "online":"https://en.wikipedia.org/wiki/National_Ranching_Heritage_Center"
      },
  ];
 
@@ -103,12 +98,12 @@ heroes.forEach((hero)=>{
         /* Metoda text() umožnuje vložení "holého" textu (odpovídá innerText() v JS) */
         $(".card-title").text(hero.name);
         $(".card-text").text(hero.biography);
-        $(".card-footer").html(`Odkaz: <a href="${hero.online}">${hero.online}</a>`);
+        $(".card-footer").html(`<div class="row"><p>Odkaz : </p><a style="padding-left: 5px;" href="${hero.online}">${hero.online}</a></div>`);
         /* Vyprázdní se oblast s galerií fotek spojených s danou osobností */
         $(".gallery").empty();
         /* V cyklu budou přidány nové bloky s fotkami osobnosti */
         for (let i = 0; i < hero.portraits.length; i++) {
-            $(".gallery").append(`<div class="col-sm-5"><a href="#"><img src="../img/${hero.portraits[i]}" alt=""style="padding-top:10px;" class="img-fluid"></a></div>`);        
+            $(".gallery").append(`<div class="col-sm-6"><a href="#"><img src="../img/${hero.portraits[i]}" alt=""style="padding-top:10px;" class="img-fluid"></a></div>`);        
         }
     }
 
